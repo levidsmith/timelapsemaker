d6b8ea25dd060b32db67d483721db70904631d91
LeviDSmith <LeviDSmith>
2015-12-13 21:43:25 -0500

    Fix filenames generated on Mac to match filenames on Windows, so that the seq_file.rb script will generate the frame folders in the correct order

---
+++ fix_mac_filenames.rb
@@ -0,0 +1,46 @@
+# Timelapse Maker
+# 2015 Levi D. Smith
+# Web: levidsmith.com
+# Twitter: @GaTechGrad
+
+# This is for date timestamp format for filename generated on the Mac
+# Assumes all FLV files are Mac files;  Should be modified to look at filename format instead
+
+require 'fileutils'
+
+
+filePath = "."
+if (ARGV[0].nil? == false)
+	filePath = ARGV[0]
+end
+
+files = Dir.entries(filePath)
+
+files.select! { | f |
+	puts "File: #{filePath}\\#{f}"
+	File.file? "#{filePath}\\#{f}"
+
+}
+
+files.select! { | f |
+#	f =~ /.*\.flv/
+	f =~ /.* .*\..*/
+}
+
+puts "Fixing #{files.size} filenames"
+
+files.each { | f |
+
+	oldFileName = "#{f}"
+	puts "Old file: #{f}"
+	
+	#Convert "YYYY-MM-DD hh-mm-ss.ext" to "YYYY-MM-DD-hhmm-ss.ext"
+	newFileName = "#{f[(0..9)]}-#{f[(11..12)]}#{f[(14..15)]}-#{f[(17..22)]}"
+	puts "New file: #{newFileName}"
+
+	puts "Moving: " + "#{filePath}\\#{oldFileName}" + " to " + "#{filePath}\\#{newFileName}"
+
+	FileUtils.mv("#{filePath}\\#{oldFileName}", "#{filePath}\\#{newFileName}")
+
+
+}
