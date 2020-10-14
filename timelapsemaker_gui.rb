1bc903b94b701c633eb6658f2c091649e488bdb8
Levi D. Smith <me@levidsmith.com>
2020-10-14 00:36:19 -0400

    Added file selector menus for picking folders and files.  Add ffmpeg command to generate timelapse video (instead of calling VirtualDub).  May need to be optimized.

--- timelapsemaker_gui.rb
+++ timelapsemaker_gui.rb
@@ -28,8 +28,16 @@ def makeWindow(config)
 	
 	textSourceFolder = Gtk::Entry.new
 	textSourceFolder.set_width_chars(50)
+	textSourceFolder.editable = false
 	textSourceFolder.text = config.source_dir
 	panelRow.add(textSourceFolder)
+	
+	buttonSelectSourceFolder = Gtk::Button.new(:label => "Select")
+	buttonSelectSourceFolder.signal_connect "clicked" do | _widget |
+		chooseSourceFolder(textSourceFolder, config)
+	end
+	panelRow.add(buttonSelectSourceFolder)
+	
 	
 	panel.add(panelRow)
 
@@ -41,8 +49,16 @@ def makeWindow(config)
 	
 	textFFMPEG = Gtk::Entry.new()
 	textFFMPEG.set_width_chars(50)
+	textFFMPEG.editable = false
 	textFFMPEG.text = config.ffmpeg_exe
 	panelRow.add(textFFMPEG)
+
+	buttonSelectFfmpegExe = Gtk::Button.new(:label => "Select")
+	buttonSelectFfmpegExe.signal_connect "clicked" do | _widget |
+		chooseFfmpegExe(textFFMPEG, config)
+	end
+	panelRow.add(buttonSelectFfmpegExe)
+
 	
 	panel.add(panelRow)
 
@@ -85,10 +101,20 @@ def makeWindow(config)
 	
 	#Should really lock this, and have the user choose file from File Selector
 	textFramesFolder = Gtk::Entry.new
+	textFramesFolder.set_width_chars(64)
+	textFramesFolder.editable = false
 	textFramesFolder.text = config.frames_dir
 	panelRow.add(textFramesFolder)
+
+	buttonSelectFramesFolder = Gtk::Button.new(:label => "Select")
+	buttonSelectFramesFolder.signal_connect "clicked" do | _widget |
+		chooseFramesFolder(textFramesFolder, config)
+	end
+	panelRow.add(buttonSelectFramesFolder)
+
+
 	
-	buttonFramesFolder = Gtk::Button.new(:label => "Open frames folder")
+	buttonFramesFolder = Gtk::Button.new(:label => "Open")
 	buttonFramesFolder.signal_connect "clicked" do | _widget |
 		system('explorer %s' % config.frames_dir)
 	end
@@ -106,10 +132,20 @@ def makeWindow(config)
 	
 	#Should really lock this, and have the user choose file from File Selector
 	textOutputFolder = Gtk::Entry.new
+	textOutputFolder.set_width_chars(64)
+	textOutputFolder.editable = false
 	textOutputFolder.text = config.out_dir
 	panelRow.add(textOutputFolder)
 
-	buttonOutputFolder = Gtk::Button.new(:label => "Open output folder")
+
+	buttonSelectOutputFolder = Gtk::Button.new(:label => "Select")
+	buttonSelectOutputFolder.signal_connect "clicked" do | _widget |
+		chooseOutputFolder(textOutputFolder, config)
+	end
+	panelRow.add(buttonSelectOutputFolder)
+
+
+	buttonOutputFolder = Gtk::Button.new(:label => "Open")
 	buttonOutputFolder.signal_connect "clicked" do | _widget |
 		system('explorer %s' % config.out_dir)
 	end
@@ -127,8 +163,17 @@ def makeWindow(config)
 	
 	textVirtualDub = Gtk::Entry.new()
 	textVirtualDub.set_width_chars(50)
+	textVirtualDub.editable = false
 	textVirtualDub.text = config.virtual_dub_exe
 	panelRow.add(textVirtualDub)
+
+
+	buttonSelectVirtualDub = Gtk::Button.new(:label => "Select")
+	buttonSelectVirtualDub.signal_connect "clicked" do | _widget |
+		chooseVirtualDubExe(textVirtualDub, config)
+	end
+	panelRow.add(buttonSelectVirtualDub)
+
 	
 	panel.add(panelRow)
 
@@ -192,14 +237,16 @@ def makeWindow(config)
 		strFirstFrameImage =  File.join(config.out_dir, "0001.png").gsub('/', '\\')
 		puts strFirstFrameImage
 
-		system(config.virtual_dub_exe + " " + strFirstFrameImage)
+#		system(config.virtual_dub_exe + " " + strFirstFrameImage)
 
-		strMessage = "Import generated files in " + config.out_dir + " into VirtualDub (http://www.virtualdub.org/) to generate an AVI"
-		md = Gtk::MessageDialog.new :parent => $window,
-				:flags => :destroy_with_parent, :type => :info,
-				:buttons_type => :close, :message => strMessage
-		md.run
-		md.destroy
+#		strMessage = "Import generated files in " + config.out_dir + " into VirtualDub (http://www.virtualdub.org/) to generate an AVI"
+#		md = Gtk::MessageDialog.new :parent => $window,
+#				:flags => :destroy_with_parent, :type => :info,
+#				:buttons_type => :close, :message => strMessage
+#		md.run
+#		md.destroy
+		createTimeLapse(config)
+
 
 		
 	end
@@ -318,6 +365,126 @@ def saveToConfig(config, inFFMPEG, inSourceFolder, inFramesFolder, inOutputFolde
 		config.frame_interval = inFrameInterval
 		config.virtual_dub_exe = inVirtualDub
 		config.writeToFile(CONFIG_FILE)
+
+end
+
+def chooseSourceFolder(text, config) 
+	fc = Gtk::FileChooserDialog.new(:title => "Select Source Folder", :parent => $window, :action => Gtk::FileChooserAction::SELECT_FOLDER)
+	fc.add_button("Select", Gtk::ResponseType::OK)
+	iResult = fc.run
+	puts "Result: #{iResult}"
+	if (iResult == Gtk::ResponseType::OK)
+#		puts "Selected: " + fc.filename
+#		text.text = fc.filename
+		strSourceDir = fc.filename
+		text.text = strSourceDir
+		config.source_dir = strSourceDir
+		
+		config.writeToFile(CONFIG_FILE)
+
+
+	end
+	
+	fc.destroy
+end
+
+def chooseFfmpegExe(text, config)
+	fc = Gtk::FileChooserDialog.new(:title => "Select FFMPEG Executable", :parent => $window, :action => Gtk::FileChooserAction::OPEN)
+	fc.add_button("Select", Gtk::ResponseType::OK)
+	
+	ff = Gtk::FileFilter.new()
+	ff.set_name("ffmpeg executable")
+	ff.add_pattern("ffmpeg.exe")
+	
+	fc.add_filter(ff)
+	iResult = fc.run
+	if (iResult == Gtk::ResponseType::OK)
+
+		strFFMPEG = fc.filename
+		text.text = strFFMPEG
+		config.ffmpeg_exe = strFFMPEG
+		
+		config.writeToFile(CONFIG_FILE)
+
+
+	end
+	
+	fc.destroy
+
+end
+
+def chooseFramesFolder(text, config) 
+	fc = Gtk::FileChooserDialog.new(:title => "Select", :parent => $window, :action => Gtk::FileChooserAction::SELECT_FOLDER)
+	fc.add_button("Select", Gtk::ResponseType::OK)
+	iResult = fc.run
+	puts "Result: #{iResult}"
+	if (iResult == Gtk::ResponseType::OK)
+		puts "Selected: " + fc.filename
+		strFramesDir = fc.filename
+		text.text = strFramesDir
+		config.frames_dir = strFramesDir
+		config.writeToFile(CONFIG_FILE)
+
+	end
+	
+	fc.destroy
+end
+
+def chooseOutputFolder(text, config) 
+	fc = Gtk::FileChooserDialog.new(:title => "Select", :parent => $window, :action => Gtk::FileChooserAction::SELECT_FOLDER)
+	fc.add_button("Select", Gtk::ResponseType::OK)
+	iResult = fc.run
+	puts "Result: #{iResult}"
+	if (iResult == Gtk::ResponseType::OK)
+		puts "Selected: " + fc.filename
+		text.text = fc.filename
+
+		strOutputDir = fc.filename
+		text.text = strOutputDir
+		config.out_dir = strOutputDir
+		
+		config.writeToFile(CONFIG_FILE)
+
+	end
+	
+	fc.destroy
+end
+
+def chooseVirtualDubExe(text, config)
+	fc = Gtk::FileChooserDialog.new(:title => "Select VirtualDub Executable", :parent => $window, :action => Gtk::FileChooserAction::OPEN)
+	fc.add_button("Select", Gtk::ResponseType::OK)
+	
+	ff = Gtk::FileFilter.new()
+	ff.set_name("VirtualDub executable")
+	ff.add_pattern("*.exe")
+	
+	fc.add_filter(ff)
+	iResult = fc.run
+	if (iResult == Gtk::ResponseType::OK)
+
+		strVirtualDub = fc.filename
+		text.text = strVirtualDub
+		config.virtual_dub_exe = strVirtualDub
+		
+		config.writeToFile(CONFIG_FILE)
+
+
+	end
+	
+	fc.destroy
+
+end
+
+def createTimeLapse(config)
+
+#	strCommand = config.ffmpeg_exe + " -r 10/1 -i " + config.out_dir + "\\%4d.png -c:v libx264 -pix_fmt rgb24 -crf 0 " + config.out_dir + "\\" + "timelapse.mp4"
+	strCommand = config.ffmpeg_exe + " -r 10/1 -i " + config.out_dir + "\\%4d.png -c:v libx264 -pix_fmt yuv420p -crf 0 " + config.out_dir + "\\" + "timelapse.mp4"
+#strCommand = config.ffmpeg_exe + " -r 10/1 -i " + config.out_dir + "\\%4d.png " + config.out_dir + "\\" + "timelapse.mp4"
+	puts strCommand
+	system(strCommand)
+
+
+#D:\ldsmith\tools\ffmpeg\bin\ffmpeg.exe -r 10/1 -i D:\ldsmith\openbroadcaster\timelapse\ld47\out\%4d.png -c:v libx264 -pix_fmt rgb24 -crf 0 D:\ldsmith\openbroadcaster\timelapse\ld47\out\out.mp4
 
 end
 
